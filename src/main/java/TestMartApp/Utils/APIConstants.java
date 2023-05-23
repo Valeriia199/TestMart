4b49415d1a819dbb08ab9f7fab5a17798f431d00
Valeriia199 <valeriiagolovina@gmail.com>
2023-05-23 15:54:16 -0500

    added APIConstants

---
+++ src/main/java/TestMartApp/Utils/APIConstants.java
@@ -0,0 +1,22 @@
+package TestMartApp.Utils;
+
+public class APIConstants {
+    public static final String BaseURI= "https://dummyjson.com/";
+    public static final String PRODUCTS = BaseURI+"products";
+    public static final String PRODUCTS_PRICE = BaseURI+"products?limit=10&skip=10&select=title,price";
+    public static final String PRODUCTS_1 = BaseURI+"products/1";
+    public static final String PRODUCTS_SEARCH_PHONE = BaseURI+"products/search?q=phone";
+    public static final String PRODUCTS_CATEGORIES = BaseURI+"products/categories";
+    public static final String PRODUCTS_SMARTPHONES = BaseURI+"products/category/smartphones";
+    public static final String CARTS = BaseURI+"carts";
+    public static final String CARTS_1 = BaseURI+"carts/1";
+    public static final String CARTS_USER_5 = BaseURI+"carts/user/5";
+    public static final String USERS = BaseURI+"users";
+    public static final String USERS_1 = BaseURI+"users/1";
+    public static final String USERS_SEARCH_JOHN = BaseURI+"users/search?q=John";
+
+    public static final String Header_Content_type = "Content-type";
+    public static final String Content_type = "application/json";
+
+    public static final String Header_Authorization = "Authorization";
+}
