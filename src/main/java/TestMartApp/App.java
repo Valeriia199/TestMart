084cbbaf8c6d068444bf342f7dfc5c058c9577ec
Valeriia199 <valeriiagolovina@gmail.com>
2023-05-24 12:36:39 -0500

    adding

--- src/main/java/TestMartApp/App.java
+++ src/main/java/TestMartApp/App.java
@@ -1,10 +1,22 @@
 package TestMartApp;
 
 import TestMartApp.Services.CartClass;
+import TestMartApp.Services.ProductClass;
+import TestMartApp.Services.UserClass;
 
 public class App {
     public static void main(String[] args) throws Exception {
         CartClass cartClass=new CartClass();
         cartClass.getAllCarts();
+        ProductClass productClass=new ProductClass();
+        productClass.getAllProducts();
+        productClass.getAllProducts(20,1,"brand,price");
+        productClass.getProduct(1);
+        productClass.searchProducts("phone");
+        productClass.getProductsByCategory("smartphones");
+        UserClass userClass=new UserClass();
+        userClass.getAllUsers();
+        userClass.getUser(1);
+        userClass.searchUsers("John");
     }
 }
